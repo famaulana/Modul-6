b0a48da74ab7bdf0187a8a547eda8d1904265f6a
Farhan <famagung>
2018-03-26 12:17:36 +0700

    DONE!!

---
+++ app/src/main/java/com/smktelkom/www/db1_farhan/MainActivity.java
@@ -0,0 +1,64 @@
+package com.smktelkom.www.db1_farhan;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+    static final int READ_BLOCK_SIZE = 100;
+    EditText et;
+    Button sv, load;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et = findViewById(R.id.editInput);
+        sv = findViewById(R.id.simpan);
+        load = findViewById(R.id.load);
+        sv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    FileOutputStream fOut = openFileOutput("Textku.txt", MODE_APPEND);
+                    fOut.write(et.getText().toString().getBytes());
+                    fOut.close();
+                    Toast.makeText(getBaseContext(), "File Disimpan", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        load.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    FileInputStream fin = openFileInput("Textku.txt");
+                    InputStreamReader isr = new InputStreamReader(fin);
+                    char[] inputBuffer = new char[READ_BLOCK_SIZE];
+                    String s = "";
+                    int charRead;
+                    while ((charRead = isr.read(inputBuffer)) > 0) {
+                        s += String.copyValueOf(inputBuffer, 0, charRead);
+                        inputBuffer = new char[READ_BLOCK_SIZE];
+                    }
+                    et.setText(s);
+                    Toast.makeText(getBaseContext(), "File Diload", Toast.LENGTH_SHORT).show();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
